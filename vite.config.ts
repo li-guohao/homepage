18554e52550bb670aa861400f53789ac75ae1bc1
li-guohao <git@liguohao.cn>
2023-11-15 12:10:16 +0800

    support dynamic logo and title

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // vite 配置
+    plugins: [vue(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            logo: env.LOGO_URL,
+            title: env.TITLE,
+          },
+        },
+      })],
+  }
 })
