58d64481b2fd95f41ef0bce2f8be5219abf967ec
li-guohao <git@liguohao.cn>
2023-11-15 12:48:58 +0800

    add scss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>APP</div>
+  <div class="container">
+    <div>APP</div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  border: 1px red solid;
+  text-align: center;
+  min-height: 20vh;
+}
+</style>
